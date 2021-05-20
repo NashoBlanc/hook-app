18a1e27ccaf3d134454cdb370c2f3cad9ec94b59
ignacioblanc <iiblanc@falabella.cl>
2021-05-20 12:05:02 -0400

    use effects with forms example

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] =  useState({ 
+        name: '',
+        email: ''
+    });
+
+    const {name, email} = formState;
+
+    
+    useEffect(() => {
+        console.log('hey')
+    }, [formState]);
+
+    useEffect(() => {
+        console.log('email cambio')
+    }, [email]);
+
+    const hadleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+    return (
+        <>
+        <h1>Simple Form</h1>
+        <div className="form-group">
+            <input 
+            type="text" 
+            name="name" 
+            className="form-control" 
+            placeholder="Nombre" 
+            autoComplete="off"
+            value={name}
+            onChange={hadleInputChange}>
+            </input>
+        </div>
+
+
+        <div className="form-group">
+            <input 
+            type="text" 
+            name="email" 
+            className="form-control" 
+            placeholder="Email@gmail.com" 
+            autoComplete="off"
+            value={email}
+            onChange={hadleInputChange}>
+            </input>
+        </div>
+        <hr></hr>
+        </>
+    )
+}
